ae643dd615e9aff90372ba19183c86bcccb8125c
Justin Formentin <40323577+justinformentin@users.noreply.github.com>
2018-09-04 18:48:43 -0400

    Edited width

--- libs/templates/app/src/components/CodeBlock/index.jsx
+++ libs/templates/app/src/components/CodeBlock/index.jsx
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
 const CodeBlock = styled.div`
-background-color: #22292f;
-border-radius: 0.25rem;
-color:#fff;
-padding: 1rem;
-width:96%;
+  background-color: #22292f;
+  border-radius: 0.25rem;
+  color:#fff;
+  padding: 1rem;
+  width:96%;
 `;
 
 export default CodeBlock;
