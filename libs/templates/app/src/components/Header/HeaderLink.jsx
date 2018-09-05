39bffe5c814e7c0d1165b5d35a6347e57ac11718
Justin Formentin <40323577+justinformentin@users.noreply.github.com>
2018-09-05 11:13:07 -0400

    Updated styling

--- libs/templates/app/src/components/Header/HeaderLink.jsx
+++ libs/templates/app/src/components/Header/HeaderLink.jsx
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-export default styled(Link)`
+	color: #eff8ff;	
 	padding: 0.5rem 1.25rem;
 	margin-left:0.5rem;
-	color: #eff8ff;
+	font-size:1.125rem;
 	text-decoration: none !important;
 	border: 1px solid;
 	border-radius: 0.25rem;
