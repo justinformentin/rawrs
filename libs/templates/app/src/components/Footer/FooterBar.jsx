5bb80ec3dbbae5fa80cdbdb2d1f29b3e0d39ff3f
Justin Formentin <40323577+justinformentin@users.noreply.github.com>
2018-09-05 11:14:43 -0400

    Updated styling

--- libs/templates/app/src/components/Footer/FooterBar.jsx
+++ libs/templates/app/src/components/Footer/FooterBar.jsx
@@ -4,13 +4,13 @@ const FooterBar = styled.div`
   clip-path: polygon(0% 100%, 100% 100%, 100% 0, 93% 20%, 87% 0, 80% 20%, 73% 0, 66% 20%, 59% 0, 51% 20%, 44% 0, 36% 20%, 29% 0, 21% 20%, 14% 0, 7% 20%, 0 0);
   background: rgb(47,58,153);
   background: linear-gradient(315deg, rgba(47,58,153,1) 34%, rgba(2,118,214,1) 100%);
-  padding: 1rem 0 0 0;
-  margin-right:5rem;
+  padding: 2rem 0 .5rem 0;
+  margin:0;
   text-align: right;
-  position: fixed;
+  position: relative;
   bottom: 0;
   color: #e3fcec;
-  width:100vw;
+  width:100%;
   height:3vh;
 `;
 
